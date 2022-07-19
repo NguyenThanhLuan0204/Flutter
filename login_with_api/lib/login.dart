4559ad611ccfe3cdff665abc2f391a2c482a945b
Nguyễn Thành Luân <nguyenthanhluantqd@gmail.com>
2022-07-19 15:52:26 +0700

    sign in with api

---
+++ login_with_api/lib/login.dart
@@ -0,0 +1,189 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:login/main.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const Login());
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isObscure = true;
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/register.png'),
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(left: 0, top: 220),
+                    child: const Text(
+                      "Welcome",
+                      style: TextStyle(color: Colors.white, fontSize: 33),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, top: 80),
+                    margin: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Username:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        TextFormField(
+                          controller: usernameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your Username',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, top: 30),
+                    margin: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Password:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: _isObscure,
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              // this button is used to toggle the password visibility
+                              suffixIcon: IconButton(
+                                  icon: Icon(_isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  })),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(100),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        var headers = {'Content-Type': 'application/json'};
+                        var request = http.Request(
+                            'POST',
+                            Uri.parse(
+                                'https://dakshow.vn/api/auth/login?locale=en'));
+                        request.body = json.encode({
+                          "usernameOrEmail": usernameController.text.toString(),
+                          "password": passwordController.text.toString(),
+                          "remember_me": true
+                        });
+                        request.headers.addAll(headers);
+
+                        http.StreamedResponse response = await request.send();
+
+                        if (response.statusCode == 200) {
+                          String jsonString =
+                              await response.stream.bytesToString();
+                          print(jsonString);
+                          var jsoncode = jsonString.substring(8, 11);
+                          int jsoncodenumber = int.parse(jsoncode);
+                          if (jsoncodenumber == 200) {
+                            _sendDataToSecondScreen(context);
+                          } else {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("Thông báo"),
+                                    content: Text(
+                                        'Thông tin đăng nhập sai, kiểm tra lại!'),
+                                    actions: [
+                                      FlatButton(
+                                        child: Text("Ok"),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      )
+                                    ],
+                                  );
+                                });
+                          }
+                        } else {
+                          print(response.reasonPhrase);
+                        }
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _sendDataToSecondScreen(BuildContext context) {
+    String usernameSent = usernameController.text;
+    String passwordSent = passwordController.text;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Home(
+            username: usernameSent,
+            password: passwordSent,
+          ),
+        ));
+  }
+}
---
+++ login_with_api/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:login/login.dart';
+
+void main() => runApp(const Login());
+
+class Home extends StatelessWidget {
+  final String username;
+  final String password;
+
+  // receive data from the FirstScreen as a parameter
+  const Home({Key? key, required this.username, required this.password})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: [
+                  const Text(
+                    'Username:',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text(
+                    username,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'Password:',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Text(
+                    password,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ]));
+  }
+}
